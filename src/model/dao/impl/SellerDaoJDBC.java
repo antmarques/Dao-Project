65e32abc02d298f09dfac74eaa59b52c0c7df56e
antmarques <antonio5784881@gmail.com>
2024-04-25 18:24:57 -0300

    [add] Create DAO Factory and SellerDaoImpl.

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,33 @@
+package model.dao.impl;
+
+import model.dao.SellerDao;
+import model.entities.SellerEntity;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDao {
+    @Override
+    public void insert(SellerEntity seller) {
+
+    }
+
+    @Override
+    public void update(SellerEntity seller) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public SellerEntity findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<SellerEntity> findAll() {
+        return null;
+    }
+}
