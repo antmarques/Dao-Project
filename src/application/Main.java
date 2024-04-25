2c088a60b4160ae99cc4f2b286761972a1caa7e6
antmarques <antonio5784881@gmail.com>
2024-04-25 13:21:32 -0300

    [add] Create SellerEntity and testing the essentials methods of class.

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,17 +2,23 @@ package application;
 
 import db.DB;
 import model.entities.DepartmentEntity;
+import model.entities.SellerEntity;
 
 import java.sql.Connection;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        DepartmentEntity de = new DepartmentEntity(1, "Antonio");
-        DepartmentEntity de2 = new DepartmentEntity(1, "Antonio");
+        DepartmentEntity de = new DepartmentEntity(1, "Hardware");
+        SellerEntity se = new SellerEntity(1, "Antonio", "antonio@email.com", new Date(), 3000.0, de);
+        DepartmentEntity de2 = new DepartmentEntity(1, "Hardware");
+        SellerEntity se2 = new SellerEntity(1, "Antonio", "antonio@email.com", new Date(), 3000.0, de2);
 
-        System.out.println(de);
-        System.out.println(de2);
-        System.out.println(de.equals(de2));
-        System.out.println(de.hashCode() + " - " + de2.hashCode());
+
+        System.out.println(se.equals(se2));
+        System.out.println(se.hashCode());
+        System.out.println(se2.hashCode());
+
     }
 }
