4e6975aa8754d96e8e229c400ccacce267770cb0
antmarques <antonio5784881@gmail.com>
2024-04-26 16:55:40 -0300

    [feature] Main program for test.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,18 +1,42 @@
 package application;
 
 import model.dao.DaoFactory;
-import model.dao.DepartmentDao;
+import model.dao.SellerDao;
 import model.entities.DepartmentEntity;
+import model.entities.SellerEntity;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        Locale.setDefault(Locale.of("pt" , "BR"));
-        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+        //Test Crud DepartmentDao!
 
-        List<DepartmentEntity> list = departmentDao.findAll();
+        Locale.setDefault(Locale.of("pt" , "BR"));
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        System.out.println("=== TEST 1: seller findById ===");
+        SellerEntity seller = sellerDao.findById(2);
+        System.out.println(seller);
+
+        System.out.println("=== TEST 2: seller findByDepartment ===");
+        DepartmentEntity de = new DepartmentEntity(1, null);
+        List<SellerEntity> list = sellerDao.findByDepartment(de);
         list.forEach(System.out::println);
+
+        System.out.println("=== TEST 3: seller findAll ===");
+        list = sellerDao.findAll();
+        list.forEach(System.out::println);
+
+        System.out.println("=== TEST 4: seller Insert ===");
+        SellerEntity newSeller = new SellerEntity(null, "Greg", "greg@gmail.com", new Date(), 4000.0, de);
+        sellerDao.insert(newSeller);
+
+        System.out.println("=== TEST 5: seller Update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Marta Wayne");
+        seller.setEmail("marta@gamil.com");
+        sellerDao.update(seller);
     }
 }
---
+++ src/application/Main2.java
@@ -0,0 +1,33 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.DepartmentEntity;
+
+import java.util.List;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.of("pt" , "BR"));
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+
+        System.out.println("=== TEST 1: department findById ===");
+        DepartmentEntity department = departmentDao.findById(2);
+        System.out.println(department);
+
+        System.out.println("=== TEST 2: department findAll ===");
+        List<DepartmentEntity>list = departmentDao.findAll();
+        list.forEach(System.out::println);
+
+        System.out.println("=== TEST 4: department Insert ===");
+        DepartmentEntity newDepartment = new DepartmentEntity(null, "Music");
+        departmentDao.insert(newDepartment);
+        System.out.println("Inserted! New id = " + newDepartment.getId());
+
+        System.out.println("=== TEST 5: department Update ===");
+        department = departmentDao.findById(1);
+        department.setName("Marta Wayne");
+        department.setEmail("marta@gamil.com");
+        departmentDao.update(department);
+    }
+}
